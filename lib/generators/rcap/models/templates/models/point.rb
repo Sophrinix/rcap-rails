8f9d5a89696134fce6ddf2d8ae657f5e1cad7c7f
Pat George <pat.george@gmail.com>
2011-03-20 09:53:45 -0400

    Update models to use ActiveModel.

--- lib/generators/rcap/models/templates/models/point.rb
+++ lib/generators/rcap/models/templates/models/point.rb
@@ -1,53 +1,51 @@
-module RCAP
-	# A Point object is valid if
-	# * it has a lattitude within the minimum and maximum lattitude values
-	# * it has a longitude within the minimum and maximum longitude values
-  class Point
-    include Validation
+# A Point object is valid if
+# * it has a latitude within the minimum and maximum latitude values
+# * it has a longitude within the minimum and maximum longitude values
+class Point < ActiveRecord::Base
+  include Validation
 
-    MAX_LONGITUDE = 180
-    MIN_LONGITUDE = -180
-    MAX_LATTITUDE = 90
-    MIN_LATTITUDE= -90
+  MAX_LONGITUDE = 180
+  MIN_LONGITUDE = -180
+  MAX_LATITUDE = 90
+  MIN_LATITUDE= -90
 
-    attr_accessor( :lattitude )
-		attr_accessor( :longitude )
+  attr_accessor( :latitude )
+	attr_accessor( :longitude )
 
-    validates_numericality_of( :lattitude, :longitude )
-    validates_inclusion_of( :lattitude, :in => MIN_LATTITUDE..MAX_LATTITUDE )
-    validates_inclusion_of( :longitude, :in => MIN_LONGITUDE..MAX_LONGITUDE)
+  validates_numericality_of( :latitude, :longitude )
+  validates_inclusion_of( :latitude, :in => MIN_LATITUDE..MAX_LATITUDE )
+  validates_inclusion_of( :longitude, :in => MIN_LONGITUDE..MAX_LONGITUDE)
 
-    def initialize( attributes = {} )
-      @lattitude = attributes[ :lattitude ]
-      @longitude = attributes[ :longitude ]
-    end
+  def initialize( attributes = {} )
+    @latitude = attributes[ :latitude ]
+    @longitude = attributes[ :longitude ]
+  end
 
-		# Returns a string representation of the point of the form
-		#  lattitude,longitude
-    def to_s
-      "#{ self.lattitude },#{ self.longitude }"
-    end
+	# Returns a string representation of the point of the form
+	#  latitude,longitude
+  def to_s
+    "#{ self.latitude },#{ self.longitude }"
+  end
 
-    def inspect # :nodoc:
-      '('+self.to_s+')'
-    end
+  def inspect # :nodoc:
+    '('+self.to_s+')'
+  end
 
-		# Two points are equivalent if they have the same lattitude and longitude
-		def ==( other )
-			[ self.lattitude, self.longitude ] == [ other.lattitude, other.longitude ]
-		end
+	# Two points are equivalent if they have the same latitude and longitude
+	def ==( other )
+		[ self.latitude, self.longitude ] == [ other.latitude, other.longitude ]
+	end
 
-    LATTITUDE_KEY = 'lattitude_hash'  # :nodoc:
-    LONGITUDE_KEY = 'longitude_hash'  # :nodoc:
+  LATITUDE_KEY = 'latitude_hash'  # :nodoc:
+  LONGITUDE_KEY = 'longitude_hash'  # :nodoc:
 
-    def to_h # :nodoc:
-      RCAP.attribute_values_to_hash(
-        [ LATTITUDE_KEY, self.lattitude ],
-        [ LONGITUDE_KEY, self.longitude ])
-    end
+  def to_h # :nodoc:
+    RCAP.attribute_values_to_hash(
+      [ LATITUDE_KEY, self.latitude ],
+      [ LONGITUDE_KEY, self.longitude ])
+  end
 
-    def self.from_h( point_hash ) # :nodoc:
-      self.new( :lattitude => point_hash[ LATTITUDE_KEY ], :longitude => point_hash[ LONGITUDE_KEY ])
-    end
+  def self.from_h( point_hash ) # :nodoc:
+    self.new( :latitude => point_hash[ LATITUDE_KEY ], :longitude => point_hash[ LONGITUDE_KEY ])
   end
 end
